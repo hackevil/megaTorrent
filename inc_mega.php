ef25624ee9670f082a15eabe4c50123081c4fb3d
dvil <dvil88@gmail.com>
2014-05-21 13:52:34 +0200

    Added progressbar to upload process

--- inc_mega.php
+++ inc_mega.php
@@ -419,7 +419,12 @@ function sendFile($uploadUrl,$fileName){
 
 	$bytes = 0;$write = substr($header,$bytes,8192);$bytes += 8192;
 	while(fwrite($fp,$write) === false){$write = substr($header,$bytes,8192);$bytes += 8192;}
-	while($block = fread($file,8192)){fwrite($fp,$block);}
+	$bytesUploaded = 0;
+	while($block = fread($file,8192)){
+		$bytesUploaded += strlen($block);
+		showStatus($bytesUploaded,$fileSize,130);
+		fwrite($fp,$block);
+	}
 	fwrite($fp,$CR.$CR);
 
 	$buffer = '';
@@ -434,5 +439,34 @@ function sendFile($uploadUrl,$fileName){
 	$content = substr($buffer,$break);
 
 	return $content;
+}
+
+function showStatus($done,$total,$size=30){
+	static $start_time;
+	if($done > $total){return;}
+	if(empty($start_time)){$start_time = time();}
+
+	$now = time();
+	$perc = (double)($done/$total);
+	$bar = floor($perc*$size);
+
+	$status_bar = "\r[";
+	$status_bar .= str_repeat('=', $bar);
+	if($bar<$size){$status_bar .= '>';$status_bar .= str_repeat(' ', $size-$bar);}
+	else{$status_bar .= '=';}
+
+	$disp = number_format($perc*100, 0);
+
+	$status_bar.="] $disp% $done/$total";
+
+	$rate = ($now-$start_time)/$done;
+	$left = $total - $done;
+	$eta = round($rate * $left, 2);
+
+	$elapsed = $now - $start_time;
+	$status_bar .= ' remaining: '.number_format($eta).' sec. elapsed: '.number_format($elapsed).' sec.';
+	echo "$status_bar ";
+	flush();
+	if($done >= $total){echo "\n";}
 }
 ?>
